fa6e941780a0fd10da851a69ed6cf7c81a1b8de7
Brian Woodward <brian.woodward@gmail.com>
2015-02-03 09:22:21 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * sub-cache <https://github.com/doowb/sub-cache>
+ *
+ * Copyright (c) 2015 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
